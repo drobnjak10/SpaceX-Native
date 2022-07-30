23ab8fe602ff372a684c98f157e465e247416540
drobnjak10 <77175544+drobnjak10@users.noreply.github.com>
2022-07-30 13:43:41 +0200

    Delete native/cats directory

--- native/cats/src/screens/CrewMember.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CrewMember = () => {
-	return (
-		<div>CrewMember</div>
-	)
-}
-
-export default CrewMember
--- native/cats/src/screens/CrewMembers.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CrewMembers = () => {
-	return (
-		<div>CrewMembers</div>
-	)
-}
-
-export default CrewMembers
